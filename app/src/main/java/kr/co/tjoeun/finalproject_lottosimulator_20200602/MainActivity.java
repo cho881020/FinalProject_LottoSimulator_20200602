6860c3896ac69b340b85e7ed6bf7a3edde65eed7
kj_cho <kj_cho@idealidea.co.kr>
2020-06-02 11:54:39 +0900

    자동 구매 중단/재개 구현  - 핸들러에게 removeCallback으로 다음 예정된 할일을 제거.  - 할일이 없어지면서 정지됨.

--- app/src/main/java/kr/co/tjoeun/finalproject_lottosimulator_20200602/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/finalproject_lottosimulator_20200602/MainActivity.java
@@ -39,6 +39,8 @@ public class MainActivity extends BaseActivity {
 
     List<TextView> myNumTxts = new ArrayList<>();
 
+    boolean isAutoBuyRunning = false;
+
     Handler mHandler = new Handler();
     Runnable buyLottoRunnable = new Runnable() {
         @Override
@@ -74,7 +76,17 @@ public class MainActivity extends BaseActivity {
             @Override
             public void onClick(View v) {
 
-                mHandler.post(buyLottoRunnable);
+                if (!isAutoBuyRunning) {
+                    mHandler.post(buyLottoRunnable);
+                    isAutoBuyRunning = true;
+                    binding.buyAutoLottoBtn.setText(getResources().getString(R.string.pause_auto_buying));
+                }
+                else {
+                    mHandler.removeCallbacks(buyLottoRunnable);
+                    isAutoBuyRunning = false;
+                    binding.buyAutoLottoBtn.setText(getResources().getString(R.string.resume_auto_buying));
+                }
+
 
             }
         });
